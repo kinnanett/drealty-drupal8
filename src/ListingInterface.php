45282712c8ad7c80d6245739424b69c15255a6e6
Darron L <kinnanett@gmail.com>
2014-08-23 15:24:55 -0300

    added listing interface

---
+++ src/ListingInterface.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\drealty\ListingInterface.
+ */
+
+namespace Drupal\drealty;
+
+use Drupal\user\EntityOwnerInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Provides an interface defining a listing entity.
+ */
+interface ListingInterface extends ContentEntityInterface, EntityChangedInterface, EntityOwnerInterface {
+
+  /**
+   * Returns the listing type.
+   *
+   * @return string
+   *   The listing type.
+   */
+  public function getType();
+
+  /**
+   * Returns the listing title.
+   *
+   * @return string
+   *   Title of the listing.
+   */
+  public function getTitle();
+
+  /**
+   * Sets the listing title.
+   *
+   * @param string $title
+   *   The listing title.
+   *
+   * @return \Drupal\drealty\ListingInterface
+   *   The called listing entity.
+   */
+  public function setTitle($title);
+
+  /**
+   * Returns the listing creation timestamp.
+   *
+   * @return int
+   *   Creation timestamp of the listing.
+   */
+  public function getCreatedTime();
+
+  /**
+   * Sets the listing creation timestamp.
+   *
+   * @param int $timestamp
+   *   The listing creation timestamp.
+   *
+   * @return \Drupal\drealty\ListingInterface
+   *   The called listing entity.
+   */
+  public function setCreatedTime($timestamp);
+
+  /**
+   * Returns the listing promotion status.
+   *
+   * @return bool
+   *   TRUE if the listing is promoted.
+   */
+  public function isPromoted();
+
+  /**
+   * Sets the listing promoted status.
+   *
+   * @param bool $promoted
+   *   TRUE to set this listing to promoted, FALSE to set it to not promoted.
+   *
+   * @return \Drupal\drealty\ListingInterface
+   *   The called listing entity.
+   */
+  public function setPromoted($promoted);
+
+  /**
+   * Returns the listing sticky status.
+   *
+   * @return bool
+   *   TRUE if the listing is sticky.
+   */
+  public function isSticky();
+
+  /**
+   * Sets the listing sticky status.
+   *
+   * @param bool $sticky
+   *   TRUE to set this listing to sticky, FALSE to set it to not sticky.
+   *
+   * @return \Drupal\drealty\ListingInterface
+   *   The called listing entity.
+   */
+  public function setSticky($sticky);
+
+  /**
+   * Returns the listing published status indicator.
+   *
+   * Unpublished listings are only visible to their authors and to administrators.
+   *
+   * @return bool
+   *   TRUE if the listing is published.
+   */
+  public function isPublished();
+
+  /**
+   * Sets the published status of a listing.
+   *
+   * @param bool $published
+   *   TRUE to set this listing to published, FALSE to set it to unpublished.
+   *
+   * @return \Drupal\drealty\ListingInterface
+   *   The called listing entity.
+   */
+  public function setPublished($published);
+
+}
