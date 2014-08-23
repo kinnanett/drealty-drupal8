8f8a7f3380744bcbf325675a0d43a89cb5ec7153
Darron L <kinnanett@gmail.com>
2014-08-23 09:30:40 -0300

    add interface for connection config entity

---
+++ src/ConnectionInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\drealty\ConnectionInterface.
+ */
+
+namespace Drupal\drealty;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface defining a DRealty Connection entity type.
+ */
+interface ConnectionInterface extends ConfigEntityInterface {
+
+}
