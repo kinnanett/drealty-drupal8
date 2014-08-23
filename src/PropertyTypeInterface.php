12955d5dcac0dc3c85f7fc85d97978c5cac21659
Darron L <kinnanett@gmail.com>
2014-08-23 13:41:08 -0300

    add property type interface

---
+++ src/PropertyTypeInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\drealty\PropertyTypeInterface
+ */
+
+namespace Drupal\drealty;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+interface PropertyTypeInterface extends ContentEntityInterface {
+
+}
