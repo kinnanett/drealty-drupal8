f676b47f80784f3c2a6ff75a3192d6d508957f53
Darron L <kinnanett@gmail.com>
2014-08-23 15:01:55 -0300

    implemented empty access controller

--- src/ListingTypeAccessController.php
+++ src/ListingTypeAccessController.php
@@ -1,2 +1,16 @@
 <?php
-// @TODO
+
+/**
+ * @file
+ * Contains \Drupal\drealty\ListingTypeAccessController
+ */
+
+namespace Drupal\drealty;
+
+use Drupal\Core\Entity\EntityAccessController;
+
+class ListingTypeAccessController extends EntityAccessController {
+
+  // @TODO use this or lose this!
+
+}
