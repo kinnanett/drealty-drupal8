770fc1429755427eca5430f79abe96613bdc02eb
Darron L <kinnanett@gmail.com>
2014-08-23 14:15:31 -0300

    complement property type interface with some stuff from node types

--- src/PropertyTypeInterface.php
+++ src/PropertyTypeInterface.php
@@ -11,4 +11,24 @@ use Drupal\Core\Entity\ContentEntityInterface;
 
 interface PropertyTypeInterface extends ContentEntityInterface {
 
+  /**
+   * Returns the configured property type settings of a given module, if any.
+   *
+   * @param string $module
+   *   The name of the module whose settings to return.
+   *
+   * @return array
+   *   An associative array containing the module's settings for the property type.
+   *   Note that this can be empty, and default values do not necessarily exist.
+   */
+  public function getModuleSettings($module);
+
+  /**
+   * Determines whether the property type is locked.
+   *
+   * @return string|false
+   *   The module name that locks the type or FALSE.
+   */
+  public function isLocked();
+
 }
