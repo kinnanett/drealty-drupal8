e0ce11b973126b9ca387f8d9f0dc384a0ee6331b
Darron L <kinnanett@gmail.com>
2014-08-23 15:38:42 -0300

    added empty listing access controller

---
+++ src/ListingAccessController.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\drealty\ListingAccessController
+ */
+
+namespace Drupal\drealty;
+
+use Drupal\Core\Entity\EntityAccessController;
+
+class ListingAccessController extends EntityAccessController {
+
+  // @TODO use this or lose this!
+
+}
