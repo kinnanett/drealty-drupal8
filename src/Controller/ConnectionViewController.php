27f466505cbc5840ae4e76154b440eef3a054121
Darron L <kinnanett@gmail.com>
2014-08-24 07:51:12 -0300

    add initial connection view controller

---
+++ src/Controller/ConnectionViewController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\drealty\Controller\ConnectionViewController.
+ */
+
+namespace Drupal\drealty\Controller;
+
+use Drupal\Component\Utility\String;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\Controller\EntityViewController;
+
+/**
+ * Defines a controller to render a single connection.
+ */
+class ConnectionViewController extends EntityViewController {
+
+}
